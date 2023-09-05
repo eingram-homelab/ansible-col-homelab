f47d9336ac0f5d9f93ce7e7917f3c47e6eb50d81
Edward Ingram <edward.ingram23@gmail.com>
2023-09-05 11:58:05 -0700

    test

--- roles/win_scripts/files/common/get-childitems.ps1
+++ roles/win_scripts/files/common/get-childitems.ps1
@@ -9,3 +9,4 @@ Select-Object FullName |
 ForEach-Object -Process{New-Object -TypeName PSObject -Property @{Name =$_.FullName;
     Size = (Get-ChildItem -path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue |
     Measure-Object -Property Length -Sum ).Sum/1024MB}} | Select-Object Name, @{Name="Size(GB)";Expression={("{0:N2}" -f($_.Size))}}
+    
